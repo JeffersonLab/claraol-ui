1d50dc16e56f538c0e33e7b17874998f53f75bb8
David Kroell <dkroell@acm.org>
2016-02-17 06:26:47 -0500

    Initialize javafx frameowrk files

---
+++ src/gui/Applic.java
@@ -0,0 +1,27 @@
+package gui;
+/**
+ * Created by davidkroell on 2/16/16.
+ *
+ *
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+public class Applic extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Clara");
+
+    }
+}
